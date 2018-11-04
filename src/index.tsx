214d00c71808f9f307cc9e842dae2b1206fba46c
whirish <lpoflynn@protonmail.ch>
2018-11-04 07:59:18 -0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import Root from './Root';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Root />, document.getElementById('root') as HTMLElement);
+registerServiceWorker();
