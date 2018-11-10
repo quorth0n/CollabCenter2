d16aa8f56f6e407bebc99065c11121d793ed0c0b
whirish <lpoflynn@protonmail.ch>
2018-11-09 22:17:03 -0800

    Working editor

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,5 @@ import * as ReactDOM from 'react-dom';
 
 import Router from './components/Router';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Router />, document.getElementById('root') as HTMLElement);
-registerServiceWorker();
+ReactDOM.render(<Router />, document.getElementById('root'));
